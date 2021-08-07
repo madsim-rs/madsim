18e3e4fb2b6ec471ade2ad09fe86965cce03659f
Runji Wang <wangrunji0408@163.com>
2021-08-08 01:22:40 +0800

    fs: fix create and add some functions

--- madsim/src/fs.rs
+++ madsim/src/fs.rs
@@ -91,6 +91,7 @@ impl FileSystemLocalHandle {
         let mut fs = self.fs.lock().unwrap();
         let inode = fs
             .entry(path.into())
+            .and_modify(|inode| inode.truncate())
             .or_insert_with(|| Arc::new(INode::new(path)))
             .clone();
         Ok(File {
@@ -112,6 +113,10 @@ impl INode {
             data: RwLock::new(Vec::new()),
         }
     }
+
+    fn truncate(&self) {
+        self.data.write().unwrap().clear();
+    }
 }
 
 pub struct File {
@@ -120,6 +125,16 @@ pub struct File {
 }
 
 impl File {
+    pub async fn open(path: impl AsRef<Path>) -> Result<File> {
+        let handle = FileSystemLocalHandle::current();
+        handle.open(path).await
+    }
+
+    pub async fn create(path: impl AsRef<Path>) -> Result<File> {
+        let handle = FileSystemLocalHandle::current();
+        handle.create(path).await
+    }
+
     pub async fn read_at(&self, buf: &mut [u8], offset: u64) -> Result<usize> {
         trace!(
             "file({:?}): read_at: offset={}, len={}",
@@ -161,11 +176,27 @@ impl File {
     }
 
     pub async fn set_len(&self, size: u64) -> Result<()> {
-        trace!("file({:?}): set_len={}", self.inode.path, size,);
+        trace!("file({:?}): set_len={}", self.inode.path, size);
         let mut data = self.inode.data.write().unwrap();
         data.resize(size as usize, 0);
+        // TODO: random delay
         Ok(())
     }
+
+    pub async fn sync_all(&self) -> Result<()> {
+        trace!("file({:?}): sync_all", self.inode.path);
+        // TODO: random delay
+        Ok(())
+    }
+}
+
+/// Read the entire contents of a file into a bytes vector.
+pub async fn read(path: impl AsRef<Path>) -> Result<Vec<u8>> {
+    let handle = FileSystemLocalHandle::current();
+    let file = handle.open(path).await?;
+    let data = file.inode.data.read().unwrap().clone();
+    // TODO: random delay
+    Ok(data)
 }
 
 #[cfg(test)]
@@ -178,12 +209,11 @@ mod tests {
         let runtime = Runtime::new();
         let host = runtime.local_handle("0.0.0.1:1".parse().unwrap());
         let f = host.spawn(async move {
-            let fs = FileSystemLocalHandle::current();
             assert_eq!(
-                fs.open("file").await.err().unwrap().kind(),
+                File::open("file").await.err().unwrap().kind(),
                 ErrorKind::NotFound
             );
-            let file = fs.create("file").await.unwrap();
+            let file = File::create("file").await.unwrap();
             file.write_all_at(b"hello", 0).await.unwrap();
 
             let mut buf = [0u8; 10];
@@ -192,11 +222,17 @@ mod tests {
             assert_eq!(&buf[..3], b"llo");
             drop(file);
 
-            let rofile = fs.open("file").await.unwrap();
+            // writing to a read-only file should be denied
+            let rofile = File::open("file").await.unwrap();
             assert_eq!(
                 rofile.write_all_at(b"gg", 0).await.err().unwrap().kind(),
                 ErrorKind::PermissionDenied
             );
+
+            // create should truncate existing file
+            let file = File::create("file").await.unwrap();
+            let read_len = file.read_at(&mut buf, 0).await.unwrap();
+            assert_eq!(read_len, 0);
         });
         runtime.block_on(f);
     }
