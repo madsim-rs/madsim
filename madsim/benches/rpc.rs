a795c840c7975d9db6b57e901534fc9584d8a895
Runji Wang <wangrunji0408@163.com>
2021-09-16 18:31:47 +0800

    add basic bench for RPC

---
+++ madsim/benches/rpc.rs
@@ -0,0 +1,65 @@
+use criterion::*;
+use madsim::{
+    net::{rpc::*, NetLocalHandle},
+    Runtime,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Req;
+impl Request for Req {
+    type Response = ();
+}
+
+fn empty_rpc(c: &mut Criterion) {
+    let runtime = Runtime::new();
+    let host = runtime.create_host("127.0.0.1:0").unwrap();
+    let addr = host.local_addr();
+    host.spawn(async move {
+        let net = NetLocalHandle::current();
+        net.add_rpc_handler(|_: Req| async move {});
+    })
+    .detach();
+
+    c.bench_function("[sim] empty RPC", |b| {
+        b.iter(|| {
+            runtime.block_on(async move {
+                let net = NetLocalHandle::current();
+                net.call(addr, Req).await.unwrap();
+            })
+        })
+    });
+}
+
+fn rpc_data(c: &mut Criterion) {
+    let runtime = Runtime::new();
+    let host = runtime.create_host("127.0.0.1:0").unwrap();
+    let addr = host.local_addr();
+    host.spawn(async move {
+        let net = NetLocalHandle::current();
+        net.add_rpc_handler_with_data(|_: Req, data| async move {
+            black_box(data);
+            ((), vec![])
+        });
+    })
+    .detach();
+
+    let mut group = c.benchmark_group("[sim] RPC with data");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    for size in [16, 256, 4096, 65536, 1048576] {
+        group.throughput(Throughput::Bytes(size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            let data = vec![0u8; size];
+            b.iter(|| {
+                runtime.block_on(async {
+                    let net = NetLocalHandle::current();
+                    net.call_with_data(addr, Req, &data).await.unwrap();
+                })
+            });
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, empty_rpc, rpc_data);
+criterion_main!(benches);
