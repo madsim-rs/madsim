733859ec7b3e802c03ee59e746bf2887989c254e
Runji Wang <wangrunji0408@163.com>
2022-05-05 13:27:32 +0800

    fix RPC bench
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- madsim/benches/rpc.rs
+++ madsim/benches/rpc.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use criterion::*;
-use madsim::{net::Endpoint, Request, Runtime};
+use madsim::{net::Endpoint, Request};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Request)]
@@ -9,33 +9,35 @@ use serde::{Deserialize, Serialize};
 struct Req;
 
 fn empty_rpc(c: &mut Criterion) {
-    let runtime = Runtime::new();
-    let node = runtime.create_node().build();
-    let rpc = node.spawn(async move {
-        let net = Arc::new(Endpoint::bind("127.0.0.1:10000").await.unwrap());
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let addr = rt.block_on(async move {
+        let net = Arc::new(Endpoint::bind("127.0.0.1:0").await.unwrap());
         net.add_rpc_handler(|_: Req| async move {});
         net.local_addr().unwrap()
     });
-    let addr = runtime.block_on(rpc);
 
     c.bench_function("empty RPC", |b| {
-        let net = runtime.block_on(Endpoint::bind("127.0.0.1:10001")).unwrap();
-        b.iter(|| runtime.block_on(net.call(addr, Req)).unwrap());
+        let net = rt.block_on(Endpoint::bind("127.0.0.1:0")).unwrap();
+        b.iter(|| rt.block_on(net.call(addr, Req)).unwrap());
     });
 }
 
 fn rpc_data(c: &mut Criterion) {
-    let runtime = Runtime::new();
-    let node = runtime.create_node().build();
-    let rpc = node.spawn(async move {
-        let net = Arc::new(Endpoint::bind("127.0.0.1:10000").await.unwrap());
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let addr = rt.block_on(async move {
+        let net = Arc::new(Endpoint::bind("127.0.0.1:0").await.unwrap());
         net.add_rpc_handler_with_data(|_: Req, data| async move {
             black_box(data);
             ((), vec![])
         });
         net.local_addr().unwrap()
     });
-    let addr = runtime.block_on(rpc);
 
     let mut group = c.benchmark_group("RPC with data");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
@@ -43,12 +45,8 @@ fn rpc_data(c: &mut Criterion) {
         group.throughput(Throughput::Bytes(size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
             let data = vec![0u8; size];
-            let net = runtime.block_on(Endpoint::bind("127.0.0.1:10001")).unwrap();
-            b.iter(|| {
-                runtime
-                    .block_on(net.call_with_data(addr, Req, &data))
-                    .unwrap()
-            });
+            let net = rt.block_on(Endpoint::bind("127.0.0.1:0")).unwrap();
+            b.iter(|| rt.block_on(net.call_with_data(addr, Req, &data)).unwrap());
         });
     }
     group.finish();
