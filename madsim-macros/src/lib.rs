4dd2f7fca111c88ee2a499191818f886f749388b
Runji Wang <wangrunji0408@163.com>
2022-05-21 14:23:28 +0800

    improve error message on panic in simulation
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- madsim-macros/src/lib.rs
+++ madsim-macros/src/lib.rs
@@ -185,8 +185,8 @@ fn parse_test(
                         rand_log = log;
                     }
                     Err(e) => {
-                        println!("MADSIM_CONFIG_HASH={:016X}", config.hash());
-                        println!("MADSIM_TEST_SEED={}", seed);
+                        println!("note: run with `MADSIM_TEST_SEED={}` environment variable to reproduce this error", seed);
+                        println!("      and make sure `MADSIM_CONFIG_HASH={:016X}`", config.hash());
                         ::std::panic::resume_unwind(e);
                     }
                 }
