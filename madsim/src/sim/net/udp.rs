ce51bdb877a98523d082575854a8fed60abfef68
Runji Wang <wangrunji0408@163.com>
2022-08-11 16:15:12 +0800

    feat(net): add basic `UdpSocket` (#40)
    * net: move Endpoint to a new mod
    
    Signed-off-by: Runji Wang <wangrunji0408@163.com>
    
    * net: add basic UdpSocket over Endpoint
    
    Signed-off-by: Runji Wang <wangrunji0408@163.com>
    
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

---
+++ madsim/src/sim/net/udp.rs
@@ -0,0 +1,57 @@
+use std::io::Result;
+use std::net::SocketAddr;
+
+use super::{lookup_host, Endpoint, ToSocketAddrs};
+
+/// A UDP socket.
+pub struct UdpSocket {
+    ep: Endpoint,
+}
+
+impl UdpSocket {
+    /// This function will create a new UDP socket and attempt to bind it to the `addr` provided.
+    pub async fn bind<A: ToSocketAddrs>(addr: A) -> Result<Self> {
+        let ep = Endpoint::bind(addr).await?;
+        Ok(UdpSocket { ep })
+    }
+
+    /// Connects the UDP socket setting the default destination for send() and limiting packets
+    /// that are read via recv from the address specified in `addr`.
+    pub async fn connect<A: ToSocketAddrs>(&self, addr: A) -> Result<()> {
+        let addr = lookup_host(addr).await?.next().unwrap();
+        *self.ep.peer.lock().unwrap() = Some(addr);
+        Ok(())
+    }
+
+    /// Returns the local socket address.
+    pub fn local_addr(&self) -> Result<SocketAddr> {
+        self.ep.local_addr()
+    }
+
+    /// Returns the socket address of the remote peer this socket was connected to.
+    pub fn peer_addr(&self) -> Result<SocketAddr> {
+        self.ep.peer_addr()
+    }
+
+    /// Sends data on the socket to the given address. On success, returns the number of bytes written.
+    pub async fn send_to(&self, dst: impl ToSocketAddrs, buf: &[u8]) -> Result<()> {
+        self.ep.send_to(dst, 0, buf).await
+    }
+
+    /// Receives a single datagram message on the socket.
+    /// On success, returns the number of bytes read and the origin.
+    pub async fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
+        self.ep.recv_from(0, buf).await
+    }
+
+    /// Sends data on the socket to the remote address that the socket is connected to.
+    pub async fn send(&self, buf: &[u8]) -> Result<()> {
+        self.ep.send(0, buf).await
+    }
+
+    /// Receives a single datagram message on the socket from the remote address to which it is connected.
+    /// On success, returns the number of bytes read.
+    pub async fn recv(&self, buf: &mut [u8]) -> Result<usize> {
+        self.ep.recv(0, buf).await
+    }
+}
