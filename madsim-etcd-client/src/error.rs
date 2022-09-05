23e1505a907347208c15ec6f68fa6fd99c0a3133
Runji Wang <wangrunji0408@163.com>
2022-09-05 11:52:57 +0800

    etcd: fix error
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- madsim-etcd-client/src/error.rs
+++ madsim-etcd-client/src/error.rs
@@ -11,16 +11,18 @@ pub enum Error {
     /// Invalid arguments
     InvalidArgs(String),
 
-    // /// Invalid URI
-    // InvalidUri(http::uri::InvalidUri),
+    /// Invalid URI
+    InvalidUri(http::uri::InvalidUri),
+
     /// IO error
     IoError(std::io::Error),
 
-    // /// Transport error
-    // TransportError(tonic::transport::Error),
+    /// Transport error
+    TransportError(tonic::transport::Error),
 
-    // /// gRPC status
-    // GRpcStatus(tonic::Status),
+    /// gRPC status
+    GRpcStatus(tonic::Status),
+
     /// Watch error
     WatchError(String),
 
@@ -33,8 +35,9 @@ pub enum Error {
     /// Election error
     ElectError(String),
 
-    // /// Invalid header value
-    // InvalidHeaderValue(http::header::InvalidHeaderValue),
+    /// Invalid header value
+    InvalidHeaderValue(http::header::InvalidHeaderValue),
+
     /// Endpoint error
     EndpointError(String),
 }
@@ -44,15 +47,15 @@ impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::InvalidArgs(e) => write!(f, "invalid arguments: {}", e),
-            // Error::InvalidUri(e) => write!(f, "invalid uri: {}", e),
+            Error::InvalidUri(e) => write!(f, "invalid uri: {}", e),
             Error::IoError(e) => write!(f, "io error: {}", e),
-            // Error::TransportError(e) => write!(f, "transport error: {}", e),
-            // Error::GRpcStatus(e) => write!(f, "grpc request error: {}", e),
+            Error::TransportError(e) => write!(f, "transport error: {}", e),
+            Error::GRpcStatus(e) => write!(f, "grpc request error: {}", e),
             Error::WatchError(e) => write!(f, "watch error: {}", e),
             Error::Utf8Error(e) => write!(f, "utf8 error: {}", e),
             Error::LeaseKeepAliveError(e) => write!(f, "lease keep alive error: {}", e),
             Error::ElectError(e) => write!(f, "election error: {}", e),
-            // Error::InvalidHeaderValue(e) => write!(f, "invalid metadata value: {}", e),
+            Error::InvalidHeaderValue(e) => write!(f, "invalid metadata value: {}", e),
             Error::EndpointError(e) => write!(f, "endpoint error: {}", e),
         }
     }
@@ -60,12 +63,12 @@ impl Display for Error {
 
 impl std::error::Error for Error {}
 
-// impl From<http::uri::InvalidUri> for Error {
-//     #[inline]
-//     fn from(e: http::uri::InvalidUri) -> Self {
-//         Error::InvalidUri(e)
-//     }
-// }
+impl From<http::uri::InvalidUri> for Error {
+    #[inline]
+    fn from(e: http::uri::InvalidUri) -> Self {
+        Error::InvalidUri(e)
+    }
+}
 
 impl From<std::io::Error> for Error {
     #[inline]
@@ -74,19 +77,19 @@ impl From<std::io::Error> for Error {
     }
 }
 
-// impl From<tonic::transport::Error> for Error {
-//     #[inline]
-//     fn from(e: tonic::transport::Error) -> Self {
-//         Error::TransportError(e)
-//     }
-// }
+impl From<tonic::transport::Error> for Error {
+    #[inline]
+    fn from(e: tonic::transport::Error) -> Self {
+        Error::TransportError(e)
+    }
+}
 
-// impl From<tonic::Status> for Error {
-//     #[inline]
-//     fn from(e: tonic::Status) -> Self {
-//         Error::GRpcStatus(e)
-//     }
-// }
+impl From<tonic::Status> for Error {
+    #[inline]
+    fn from(e: tonic::Status) -> Self {
+        Error::GRpcStatus(e)
+    }
+}
 
 impl From<Utf8Error> for Error {
     #[inline]
@@ -95,9 +98,9 @@ impl From<Utf8Error> for Error {
     }
 }
 
-// impl From<http::header::InvalidHeaderValue> for Error {
-//     #[inline]
-//     fn from(e: http::header::InvalidHeaderValue) -> Self {
-//         Error::InvalidHeaderValue(e)
-//     }
-// }
+impl From<http::header::InvalidHeaderValue> for Error {
+    #[inline]
+    fn from(e: http::header::InvalidHeaderValue) -> Self {
+        Error::InvalidHeaderValue(e)
+    }
+}
