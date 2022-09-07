e80c3c8645442c8169fc9af37a86c08c22d3e026
Runji Wang <wangrunji0408@163.com>
2022-09-07 11:15:51 +0800

    tonic: log on rpc start and end
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- madsim-tonic/src/transport/server.rs
+++ madsim-tonic/src/transport/server.rs
@@ -220,7 +220,7 @@ impl<L> Router<L> {
                 .downcast::<(PathAndQuery, BoxMessage)>()
                 .expect("invalid type");
             let span = debug_span!("request", ?addr, ?path);
-            debug!(parent: &span, "received request");
+            debug!(parent: &span, "received");
 
             let requests: BoxMessageStream = if msg.downcast_ref::<()>().is_none() {
                 // single request
@@ -242,7 +242,7 @@ impl<L> Router<L> {
             poll_fn(|cx| svc.poll_ready(cx)).await.unwrap();
             let rsp_future = svc.call((addr, path, requests));
             madsim::task::spawn(async move {
-                let mut stream = rsp_future.instrument(span).await.unwrap();
+                let mut stream = rsp_future.instrument(span.clone()).await.unwrap();
                 // send the response
                 while let Some(rsp) = stream.next().await {
                     // rsp: Result<BoxMessage, Status>
@@ -252,6 +252,7 @@ impl<L> Router<L> {
                         return;
                     }
                 }
+                debug!(parent: &span, "completed");
             });
         }
     }
