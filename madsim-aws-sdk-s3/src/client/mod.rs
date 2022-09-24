2f4d2d594a37129c4b80bcd402682f7bfc27fe11
Kevin Axel <kevinaxel@163.com>
2022-09-24 14:01:39 +0800

    seperate client and server
    Signed-off-by: Kevin Axel <kevinaxel@163.com>

---
+++ madsim-aws-sdk-s3/src/client/mod.rs
@@ -0,0 +1,23 @@
+pub mod client;
+pub use client::*;
+pub mod config;
+pub use config::Config;
+pub mod aws_endpoint;
+pub mod error;
+pub mod input;
+pub mod model;
+pub mod no_credentials;
+pub mod operation;
+pub mod output;
+
+pub use aws_smithy_http::endpoint::Endpoint;
+pub use aws_types::region::Region;
+pub use aws_types::Credentials;
+
+pub mod types {
+    pub use aws_smithy_http::byte_stream::AggregatedBytes;
+    pub use aws_smithy_http::byte_stream::ByteStream;
+    pub use aws_smithy_http::result::SdkError;
+    pub use aws_smithy_types::Blob;
+    pub use aws_smithy_types::DateTime;
+}
