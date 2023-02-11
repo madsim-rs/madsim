a4b5367c4fa5c1ef95882b72ff75c09495a248f6
Runji Wang <wangrunji0408@163.com>
2023-02-11 11:26:44 +0800

    remove aws-config and aws-types
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- madsim-aws-config/src/meta/credentials/mod.rs
+++
@@ -1,8 +0,0 @@
-mod chain;
-pub use chain::CredentialsProviderChain;
-
-mod credential_fn;
-pub use credential_fn::provide_credentials_fn;
-
-pub mod lazy_caching;
-pub use lazy_caching::LazyCachingCredentialsProvider;
