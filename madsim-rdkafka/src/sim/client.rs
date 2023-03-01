4b6102e93e92f1f6d1c3f3354add69156c84246a
Runji Wang <wangrunji0408@163.com>
2023-03-01 18:02:15 +0800

    rdkafka: add missing API
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- madsim-rdkafka/src/sim/client.rs
+++ madsim-rdkafka/src/sim/client.rs
@@ -1,8 +1,22 @@
 /// Client-level context.
-pub trait ClientContext: Send + Sync + 'static {}
+pub trait ClientContext: Send + Sync + 'static {
+    fn rewrite_broker_addr(&self, addr: BrokerAddr) -> BrokerAddr {
+        addr
+    }
+}
 
 /// An empty [`ClientContext`] that can be used when no customizations are needed.
 #[derive(Clone, Debug, Default)]
 pub struct DefaultClientContext;
 
 impl ClientContext for DefaultClientContext {}
+
+/// Describes the address of a broker in a Kafka cluster.
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct BrokerAddr {
+    /// The host name.
+    pub host: String,
+    /// The port, either as a decimal number or the name of a service in
+    /// the services database.
+    pub port: String,
+}
