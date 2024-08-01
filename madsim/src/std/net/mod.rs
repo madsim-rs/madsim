e5ebf70f9f6b0459644f9c47a03d8efda7022ff7
Bugen Zhao <i@bugenzhao.com>
2024-08-01 12:12:33 +0800

    disable erpc feature
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- madsim/src/std/net/mod.rs
+++ madsim/src/std/net/mod.rs
@@ -30,9 +30,11 @@
 //! });
 //! ```
 
-#[cfg(feature = "erpc")]
-pub use self::erpc::*;
-#[cfg(all(not(feature = "ucx"), not(feature = "erpc")))]
+// #[cfg(feature = "erpc")]
+// pub use self::erpc::*;
+#[cfg(all(not(feature = "ucx"),
+//  not(feature = "erpc")
+))]
 pub use self::tcp::*;
 #[cfg(feature = "ucx")]
 pub use self::ucx::*;
@@ -40,13 +42,15 @@ pub use self::ucx::*;
 #[cfg(feature = "rpc")]
 #[cfg_attr(docsrs, doc(cfg(feature = "rpc")))]
 pub mod rpc;
-#[cfg(all(not(feature = "ucx"), not(feature = "erpc")))]
+#[cfg(all(not(feature = "ucx"),
+//  not(feature = "erpc")
+))]
 mod tcp;
 #[cfg(feature = "ucx")]
 mod ucx;
 
-#[cfg(feature = "erpc")]
-mod erpc;
+// #[cfg(feature = "erpc")]
+// mod erpc;
 
 #[cfg(test)]
 mod tests {
