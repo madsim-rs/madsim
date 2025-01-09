dd2d86367baac5ce546d7891813cbb4a1455e00d
xxchan <xxchan22f@gmail.com>
2025-01-09 14:56:13 +0800

    clippy
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- madsim-rdkafka/src/std/admin.rs
+++ madsim-rdkafka/src/std/admin.rs
@@ -867,7 +867,7 @@ impl<'a> NewPartitions<'a> {
 
     /// Sets the partition replica assignment for the new partitions. Only
     /// assignments for newly created replicas should be included.
-    pub fn assign(mut self, assignment: PartitionAssignment<'a>) -> NewPartitions<'_> {
+    pub fn assign(mut self, assignment: PartitionAssignment<'a>) -> NewPartitions<'a> {
         self.assignment = Some(assignment);
         self
     }
